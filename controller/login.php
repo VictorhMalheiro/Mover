6f62fcde913b55e09f9c3f60a93549643f0a635d
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2019-11-05 12:20:44 -0300

    Login

--- controller/login.php
+++ controller/login.php
@@ -1,20 +1,35 @@
-<?php
-include("blocos.php");
-func_header();
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/main.css">
+
+</head>
+
+<body>
+    <?php
   
-  if(isset($_SESSION['email']) && (isset($_SESSION['senha']))){
-    header("Location: painel.php");exit;
-  }  
-  include("conexao.php");
+  include_once("conexao.php");
+  
+  if(isset($_SESSION['login']) && (isset($_SESSION['senha']))){
+    header("Location: ./../dashboard.php");exit;
+  }
   if(isset($_POST['logar'])){
-    $email = trim(strip_tags($_POST['email']));
+    $login = trim(strip_tags($_POST['login']));
     $senha = trim(strip_tags($_POST['senha']));
 
-    $buscausuario = "SELECT * FROM `tb_usuario` WHERE email = :email and senha = :senha";
-
+    $buscausuario = "SELECT * FROM `tab_usuario` WHERE login = :login and senha = :senha";
+    
     try{
         $resultado = $conn->prepare($buscausuario);
-        $resultado->bindParam(':email', $email, PDO::PARAM_STR);
+        $resultado->bindParam(':login', $login, PDO::PARAM_STR);
         $resultado->bindParam(':senha', $senha, PDO::PARAM_STR);
         $resultado->execute();
 
@@ -22,14 +37,44 @@ func_header();
         $linha = $resultado->fetch(PDO::FETCH_ASSOC);
         
         if($contar>0){
-            $email = $_POST['email'];
+            $login = $_POST['login'];
             $senha = $_POST['senha'];
-            header("Refresh: 3, painel.php"); exit;
+            ?>
+              <section class="generic-banner relative">
+                <div class="container pt-30">
+                    <div class="row align-items-center justify-content-center">
+                      <h2 class="text-white text-center">Aguarde</h2>
+                    </div>
+                    <div class="row generic-banner-content align-items-center justify-content-center">
+                      <div class='lds-ellipsis'>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </div>
+                </div>
+            </section>
+            <?php
+            header("Refresh: 2, ./../dashboard.php"); exit;
         }else{
-         echo "<div class='alert alert-danger'>Falha ao logar! Os dados estão incorretos!</div>";
+          ?>
+              <section class="generic-banner relative">
+                <div class="container pt-30">
+                    <div class="row align-items-center justify-content-center">
+                      <h2 class="text-white text-center">Dados incorretos</h2>
+                    </div>
+                </div>
+            </section>
+            <?php
+         header("Refresh: 2, ./../login.php"); exit;
         }
       }catch(PDOException $e){
         echo $e;
       }
     }
 ?>
+
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -17,7 +17,8 @@
 		<title>Real Estate</title>
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
-			<!--
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!--
 			CSS
 			============================================= -->
 			<link rel="stylesheet" href="css/linearicons.css">
@@ -27,16 +28,24 @@
 		    <link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.css" />
 			<link rel="stylesheet" href="css/bootstrap.css">
 			<link rel="stylesheet" href="css/main.css">
+			
+			
+			
 		</head>
+		<?php
+			// INCLUDES PHP
+			include_once("./theme/header.php");
+			include_once("./theme/footer.php");
+		?>
 		<body>
 
 			<section class="generic-banner relative">
 				<!-- START HEADER -->
 				<?php
-					include_once("./theme/header.php");
 					__header();
 				?>
 				<!-- END HEADER -->
+			
 			<div class="container pt-30">
 				<div class="row height align-items-center justify-content-center">
 					<div class="col-lg-10">
@@ -45,20 +54,24 @@
 							<p class="text-white">
 								Faça login e gerencie sua conta.
 							</p>
-							<div class="">
-								<form action="#">
+							<div>
+								<form onsubmit="return logar()" method="POST" action="controller/login.php" >
 									<div class="mt-10">
-										<input type="text" name="first_name" placeholder="Usuário" onfocus="this.placeholder = ''" onblur="this.placeholder = 'usuario'" required="" class="single-input">
+										<input type="text" onblur="" name="login" placeholder="Usuário" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Usuário'" required="" class="single-input" autocomplete="username">
 									</div>
 									<div class="mt-10"></div>
-										<input type="password" name="first_name" placeholder="Senha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'password'" required="" class="single-input">
+										<input type="password" name="senha" placeholder="Senha" autocomplete="current-password" onfocus="this.placeholder = ''"  required="" class="single-input">
 									</div>
-									<div class="mt-10">
-										<input type="submit" class="genric-btn disable circle" value="Entrar">
+									<div class="mt-2 row align-items-center justify-content-center">
+										<input type="submit"  name="logar" class="genric-btn success circle" value="Entrar">
 									</div>
+									
 								</form>
-							</div>							
-						</div>							
+								<div class="mt-2 row align-items-center justify-content-center">
+									<div id="resultado"></div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -68,11 +81,15 @@
 			<div class="main-wrapper">
 				<!-- FOOTER START -->
 				<?php
-					include_once("./theme/footer.php");
 					__footer();
 				?>
 				<!-- END FOOTER -->
 			</div>
+
+			
+			<!-- <script src="https://unpkg.com/popper.js"></script>
+			<script src="https://unpkg.com/tooltip.js"></script> -->
+			<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
 			<script src="js/vendor/jquery-2.2.4.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
@@ -83,6 +100,19 @@
 			<script src="js/ion.rangeSlider.js"></script>
 			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 			<script src="js/jquery.magnific-popup.min.js"></script>
-			<script src="js/main.js"></script>	
+			<script src="js/main.js"></script>
+			<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+			<script>
+				function logar(){
+					var login = $("input[name=login]").val();
+					var senha = $("input[name=senha]").val();
+					
+					$.post("controller/login.php", { login: login, senha: senha },
+						function(data){
+							$("#resultado").html(data)
+					})
+					return false
+				}
+			</script>
 	</body>
 </html>
